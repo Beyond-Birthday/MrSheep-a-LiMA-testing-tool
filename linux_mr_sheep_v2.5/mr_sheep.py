e53e890ae286f15820011a6667607d0e0da4fbc9
Jeremy Roussey <jeremyroussey@yahoo.fr>
2017-05-15 13:46:01 +0100

    MrSheep.py commented + security for names

--- linux_mr_sheep_v2.5/mr_sheep.py
+++ linux_mr_sheep_v2.5/mr_sheep.py
@@ -9,36 +9,40 @@ Created on Fri Apr 21 12:27:58 2017
 import datetime
 import os
 from pyvirtualdisplay import Display
-import shutil
 import sys
 import time
 import unittest
 
-TITLE = True
-TEST_CLASSES = []
-MODE = "RUN"
+#------------VARIABLES ----------
+TITLE = True                    #Check if title is on
+TEST_CLASSES = []               #All tests classes
+MODE = "RUN"                    #Current mode (RUN, COMPARE or SOURCE), default = RUN
 
+#----------------------SHEEP UTILS CLASS-------------------------
+#Has all useful methods for this script
 
 class sheepUtils():
     def list_test_classes(self) :
+    #List all test classes in TestClasses folder
         for i in os.listdir('TestClasses/') :
             if 'test' in i : print(i[:(len(i)-3)])
         sys.exit(0)
 
     def find_test_class(self, name) :
+    #Search for a certain testClass in the TestClasses folder
         for i in os.listdir('TestClasses/') :
-            if ('test' in i and i[:(len(i)-3)] == name) :
+            if ('test' in i and i[:(len(i)-3)] == name) : #Check if 'test' is in the name
+            #and check if the name match with a filename without the '.py'
+
                 return True
         return False
 
     def generate_directory(self) :
+        #Generate a directory for the output
         directory = str((datetime.datetime.date(datetime.datetime.now())).strftime('%d-%m-%Y'))
-        if(MODE == "SOURCE") :
-            directory = MODE + "-" + directory
-        else :
-            directory = MODE + "-" + directory
+        directory = MODE + "-" + directory #Give a name to that new directory
         for i in TEST_CLASSES :
-            directory = directory + "-" + i
+            directory = directory + "-" + i #Add all test-classes in the name of the directory
         directory = 'Results/'+directory
         dir_path = os.path.dirname(os.path.realpath(__file__))
         dir_path = str(dir_path) + '/' + directory
@@ -48,13 +52,23 @@ class sheepUtils():
             i = 1
             new_path = dir_path
             while(os.path.exists(new_path)) :
-                new_path = dir_path + "- (" + str(i) + ")"
+                new_path = dir_path + "- (" + str(i) + ")" #add a '(1)' or etc.. if the folder already exists
                 i += 1
             os.makedirs(new_path)
 
-
+    def security_name(self) :
+        #test if folder name is under 255bytes / 255chars = max lenght for folder name
+        all_char_len = 0
+        for tc in TEST_CLASSES :
+            all_char_len += len(tc)
+        if(all_char_len >= 255-22-len(TEST_CLASSES)) : #testClasses names + example : "SOURCE-01-01-1997-(1)" + len(TESTCLASSE)
+            print("Error, too many classes...")
+            print()
+            print("Program will end now")
+            sys.exit(0)
 
 def title() :
+    #Print the title
     if(TITLE == True) :
         print("""
 ################################################
@@ -74,6 +88,7 @@ def title() :
 
 
 def usage() :
+    #print the usage of the script
     print("Here are the usages for this program :")
     print()
     print("-b will use buffer option from unittest")
@@ -92,8 +107,8 @@ def usage() :
     time.sleep(10)
     sys.exit(0)
 
-
 def command_parse() :
+    #parse the arguments
     global TEST_CLASSES
     global MODE
     global TITLE
@@ -119,11 +134,14 @@ def command_parse() :
             else :
                 print("testClass not found : " + i)
         TEST_CLASSES.sort()
+        utils.security_name()     #Test if folder name is under 255 chars
+
 
 
 
 
 def main() :
+    #Main method, print the title and run the tests according to the selected mode
     title()
 
     if(MODE == "RUN" ) :
